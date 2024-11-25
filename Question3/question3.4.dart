b899ef5ddaca1168bdb7bcca734d9c6b5f3ed988
unknown <phanuponkc65>
2024-11-25 15:41:11 +0700

    week2

--- Question3/question3.4.dart
+++ Question3/question3.4.dart
@@ -1,7 +1,15 @@
+import 'dart:io';
+
 double calculateArea({double length = 1, double width = 1}) {
   return length * width;
 }
+
 void main() {
-  print(calculateArea(length: 5, width: 4)); 
-  print(calculateArea()); 
+  print("Enter the length :");
+  double length = double.parse(stdin.readLineSync()!);
+
+  print("Enter the width :");
+  double width = double.parse(stdin.readLineSync()!);
+
+  print("The area : ${calculateArea(length: length, width: width)}");
 }
--- Question3/question3.5.dart
+++ Question3/question3.5.dart
@@ -1,19 +1,11 @@
 import 'dart:io';
 import 'dart:math';
 
-String generateRandomPassword(int length) {
-  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  final random = Random();
-  return List.generate(length, (index) => characters[random.nextInt(characters.length)]).join();
-}
-
 void main() {
-  print("Enter the length of the password (a number):");
-  int length = int.parse(stdin.readLineSync()!); // รับค่าความยาวจากผู้ใช้
+  print("Enter password length:");
+  int length = int.parse(stdin.readLineSync()!);
 
-  if (length <= 0) {
-    print("Please enter a positive number greater than 0.");
-  } else {
-    print("Your random password is: ${generateRandomPassword(length)}");
-  }
+  String password = List.generate(length, (i) => Random().nextInt(10).toString()).join();
+
+  print("password: $password");
 }
